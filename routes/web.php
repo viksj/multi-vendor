fbac93ddb0bf7ef006edd2c45a37a6dabce9d435
viks <viksjangid1@.com>
2022-10-09 19:02:21 +0530

    Covert html template to laravel template admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+// Admin Dashboard Route without admin group
+Route::get('admin/dashboard',[AdminController::class,'dashboard']);
